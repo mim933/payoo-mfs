d620777e00815590245d70e4246ba8bb0312718e
mim933 <mdyousufmim933@gmail.com>
2024-09-19 20:21:11 +0600

    added new styles

--- js/login.js
+++ js/login.js
@@ -1,4 +1,18 @@
 // console.log('button clicking file added')
-document.getElementById('button-login').addEventListener('click',function(){
-    console.log('login button clicked')
+document.getElementById('button-login').addEventListener('click', function(event){
+    event.preventDefault();
+   
+    const phoneNumber = document.getElementById('Phone-number').value;
+    const pinNumber = document.getElementById('pin-number').value;
+    console.log(phoneNumber,pinNumber);
+
+
+
+    //  this not the good way 
+    if(phoneNumber === '5' && pinNumber=== '1234'){
+        console.log('You are logged in')
+        window.location.href ='./home.html'
+    }else{
+        alert('wrong phone number or pin.');
+    }
 })
